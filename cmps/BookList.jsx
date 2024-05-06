13c58b5b86787299fbc491a151ab63e6759687b4
Belabel588 <orih1402@gmail.com>
2024-05-06 11:25:52 +0300

    render data as normal data - not JSON

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -2,7 +2,11 @@ export function BookList({ books }) {
   return <section>
     <h2>
       Books
-      <pre>{JSON.stringify(books, null, 2)}</pre>
+      <ul>
+
+        {books.map(book => <li key={book.id}>{book.title}</li>)
+        /* <pre>{JSON.stringify(books, null, 2)}</pre> */}
+      </ul>
     </h2>
   </section>
 }
