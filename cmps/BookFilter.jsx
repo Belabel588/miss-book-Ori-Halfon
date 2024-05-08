b741fc75fab08255f40220f0e48b3bbf12ab150e
Belabel588 <orih1402@gmail.com>
2024-05-08 13:04:04 +0300

    create one function for both filters and - two way data binding

--- cmps/BookFilter.jsx
+++ cmps/BookFilter.jsx
@@ -7,16 +7,22 @@ export function BookFilter() {
   const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
   // console.log('filterBy:', filterBy)
 
-  function onTxtChange({ target }) {
-    setFilterBy(prevFilterby => ({ ...prevFilterby, title: target.value }))
-  }
-  function onPriceChange({ target }) {
-    setFilterBy(prevFilterby => ({ ...prevFilterby, minPrice: +target.value }))
+  // function onTxtChange({ target }) {
+  //   setFilterBy(prevFilterby => ({ ...prevFilterby, title: target.value }))
+  // }
+  // function onPriceChange({ target }) {
+  //   setFilterBy(prevFilterby => ({ ...prevFilterby, minPrice: +target.value }))
+  // }
+
+  function handleChange() {
+    const { name, type } = target
+    const value = (type === 'number') ? +target.value : target.value
+    setFilterBy(prevFilterby => ({ ...prevFilterby, [name]: value }))
   }
 
   return <section className='book-filter'>
     <h3>Filter</h3>
-    <input onChange={onTxtChange} value={filterBy.title} type="text" placeholder="Search by title" />
-    <input onChange={onPriceChange} value={filterBy.minPrice} type="number" placeholder="Min price" />
+    <input onChange={handleChange} value={filterBy.title} name="txt" type="text" placeholder="Search by title" />
+    <input onChange={handleChange} value={filterBy.minPrice} name="minPrice" type="number" placeholder="Min price" />
   </section>
 }
